1b3b6e52ab01f88079550e16a81c03c917b2f904
suleimanKarim <s.karimeddin@innopolis.university>
2023-09-17 02:37:48 +0300

    seo.02

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,33 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Suleiman Karim Eddin",
-  description: "Suleiman Karim Eddin portfolio",
-  keywords: ["suleiman", "suleiman karim eddin", "fullstack", "frontend"],
-};
+  description: "Explore Suleiman Karim Eddin's portfolio showcasing expertise in fullstack and frontend development.",
+  keywords: [
+    "Suleiman Karim Eddin",
+    "Fullstack Developer",
+    "Frontend Developer",
+    "Web Developer",
+    "Portfolio",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "HTML",
+    "CSS",
+  ],  
+applicationName:"Suleiman Karim Eddin",
+abstract:"Explore Suleiman Karim Eddin's portfolio showcasing expertise",
+publisher:"Suleiman Karim Eddin",
+  robots: "index, follow",
 
+
+};
 export default function RootLayout({
   children,
 }: {
@@ -22,6 +39,14 @@ export default function RootLayout({
       className="snap-y snap-mandatory scrollbar-thin
        scrollbar-thumb-gray-900 scrollbar-track-gray-100  overflow-y-scroll overflow-x-hidden"
     >
+        <Head>
+        <title>Suleiman Karim Eddin </title>
+        <link
+          rel="canonical"
+          href="https://suleimna-karim-eddin.vercel.app/"
+          key="canonical"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
